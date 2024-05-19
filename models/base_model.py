6acd80d8e43bea8c6ba1889c28fd5d6ff6cb65bb
Kalkidan-Amare <kalkidanamare11a@gmail.com>
2024-05-19 15:31:05 -0700

    fixing base model

--- models/base_model.py
+++ models/base_model.py
@@ -1,52 +1,53 @@
 #!/usr/bin/python3
-"""define all common attribute/method for other classes"""
+"""Define all common attributes/methods for other classes"""
 from datetime import datetime
 from uuid import uuid4
 import models
 
 
 class BaseModel:
-    """parent class of AirBnB project"""
+    """Parent class of the AirBnB project"""
 
     def __init__(self, *args, **kwargs):
-        """initialize id, created_at, updated_at
+        """Initialize id, created_at, updated_at
         Args:
             *args (any): Unused.
             **kwargs (dict): Key/value pairs of attributes.
         """
-        time_form = "%Y-%m-%dT%H:%M:%S.%f"
+        time_format = "%Y-%m-%dT%H:%M:%S.%f"
 
-        self.id = str(uuid4())
-        self.created_at = datetime.today()
-        self.updated_at = datetime.today()
+        self.unique_id = str(uuid4())
+        self.creation_time = datetime.today()
+        self.modification_time = datetime.today()
         if len(kwargs) != 0:
-            for key, val in kwargs.items():
+            for key, value in kwargs.items():
                 if key == "__class__":
                     continue
                 if key == "created_at" or key == "updated_at":
-                    self.__dict__[key] = datetime.strptime(val, time_form)
+                    self.__dict__[key] = datetime.strptime(value, time_format)
                 else:
-                    self.__dict__[key] = val
+                    self.__dict__[key] = value
         else:
             models.storage.new(self)
 
     def save(self):
-        """updates the updated_at public instance attribute"""
-        self.updated_at = datetime.today()
+        """Updates the modification_time public instance attribute"""
+        self.modification_time = datetime.today()
         models.storage.save()
 
     def to_dict(self):
         """
-        returns dictionary of key/value of __dict__
-        key __class__ added to the dictionary
+        Returns a dictionary of key/value pairs of __dict__
+        Key __class__ added to the dictionary
         """
-        dicti = self.__dict__.copy()
-        dicti["created_at"] = self.created_at.isoformat()
-        dicti["updated_at"] = self.updated_at.isoformat()
-        dicti["__class__"] = self.__class__.__name__
+        dictionary_copy = self.__dict__.copy()
+        dictionary_copy["created_at"] = self.creation_time.isoformat()
+        dictionary_copy["updated_at"] = self.modification_time.isoformat()
+        dictionary_copy["__class__"] = self.__class__.__name__
 
-        return dicti
+        return dictionary_copy
 
     def __str__(self):
-        """print in a format"""
-        return f"[{self.__class__.__name__}] ({self.id}) {self.__dict__}"
+        """Print in a specific format"""
+        return f"[{self.__class__.__name__}] ({self.unique_id}) {self.__dict__}"
+
